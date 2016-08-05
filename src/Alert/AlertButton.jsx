c0ee98c02aacfb1869e1ad638a97e4c84273c9d7
deleav <a42006310@gmail.com>
2016-08-05 16:14:40 +0800

    update

--- src/Alert/AlertButton.jsx
+++ src/Alert/AlertButton.jsx
@@ -5,14 +5,26 @@ class AlertButton extends React.Component {
     super(props);
     this.alertText = this.alertText.bind(this);
   }
+
   alertText() {
     alert(this.props.text);
   }
+
   render() {
     return (
       <button onClick={this.alertText}>我是一個 {this.props.text} 按鈕</button>
     );
   }
+}
+
+AlertButton.propTypes = {
+  text: React.PropTypes.string,
+  num: React.PropTypes.number
+}
+
+AlertButton.defaultProps = {
+  text: 'default text',
+  num: 0
 }
 
 module.exports = AlertButton;
--- src/Alert/AlertPage.jsx
+++ src/Alert/AlertPage.jsx
@@ -3,13 +3,19 @@ var AlertButton = require('Alert/AlertButton.jsx');
 
 class AlertPage extends React.Component {
   render() {
+    let texts = ['hello', 'zet', 'moomoo'];
     return (
       <div>
-        <AlertButton text='hello'/>
+        {texts.map(t => <AlertButton text={t} />)}
+        {
+          texts.length >= 3 ? <button>true</button> : <button>false</button>
+        }
+        {/*<AlertButton text='hello'/>
         <AlertButton text='zet'/>
-        <AlertButton text='moomoo'/>
+        <AlertButton text='moomoo'/>*/}
       </div>
     );
   }
 }
+
 module.exports = AlertPage;
